7815ac5d50a66a9d56d6d28c5392737c7a25e03e
Vinicius Shiroma <vinicius.katsuaki13@hotmail.com>
2023-11-16 10:56:04 -0300

    typescript rodando

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const conn = require('./db/connection')
+
+const routes = require('./routes/router')
+
+app.use(cors())
+app.use(express.json())
+
+conn()
+
+app.use('/api', routes)
+
+app.listen(3000, () => {
+  console.log('Server Running!')
+})
